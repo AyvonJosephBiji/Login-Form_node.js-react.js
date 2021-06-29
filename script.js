0438989a7813d24c5fc827e88cacd126f051b128
Ayvon Joseph Biji <68387856+AyvonJosephBiji@users.noreply.github.com>
2021-06-29 11:18:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static("views"));
+
+
+const emailDB = "ayvon@gmail.com";
+const passwordDB = "ayvon";
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === emailDB && password === passwordDB) {
+        res.send("Login Successful");
+    } else {
+        res.send("Login Failed");
+
+    }
+
+});
+
+
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT);
